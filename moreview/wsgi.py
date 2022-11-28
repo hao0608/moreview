03ece817b35533ead56746d8fac431aeb832f8f0
YungLin-CHU <lexlie.yunglinchu@gmail.com>
2022-11-28 10:25:16 +0800

    feat: install code formatter - black and format existing files

--- moreview/wsgi.py
+++ moreview/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moreview.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moreview.settings")
 
 application = get_wsgi_application()
