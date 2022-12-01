76b6d768305c6f357a7e24f4a5331172686c944f
hsiuchun <joyce111423048@g.ncu.edu.tw>
2022-12-01 17:09:42 +0800

    navbar sample

---
+++ static/css/custom.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById('hamburger');
+
+btn.addEventListener('click', function() {
+    let top_login = document.getElementById('top_login');
+    top_login.innerHTML = ''
+    let in_login = document.getElementById('in_login');
+})
