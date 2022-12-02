a9c7abd9db66a29622bfdcd3691242a9a3a530fe
hao0608 <hao0608@users.noreply.github.com>
2022-12-02 13:29:17 +0000

    style: apply linting changes

--- moreview/urls.py
+++ moreview/urls.py
@@ -27,10 +27,9 @@ from movie.views import (
 urlpatterns = [
     path("base", TemplateView.as_view(template_name="base.html")),
     path("admin/", admin.site.urls),
-    path("movies/create", MovieCreateView.as_view(), name='movie_create_form'),
-    path("", MovieListView.as_view(), name='movie_list'),
-    path("movies/<int:pk>", MovieDetailView.as_view(), name='movie_detail'),
-    path('movies/<int:pk>/edit', MovieEditView.as_view(), name='movie_edit'),
-    path('movies/<int:pk>/delete', MovieDeleteView.as_view(), name='movie_delete'),
-    
+    path("movies/create", MovieCreateView.as_view(), name="movie_create_form"),
+    path("", MovieListView.as_view(), name="movie_list"),
+    path("movies/<int:pk>", MovieDetailView.as_view(), name="movie_detail"),
+    path("movies/<int:pk>/edit", MovieEditView.as_view(), name="movie_edit"),
+    path("movies/<int:pk>/delete", MovieDeleteView.as_view(), name="movie_delete"),
 ]
