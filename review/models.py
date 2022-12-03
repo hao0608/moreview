98aa5c8c818e953f4fc19c3fa132659f554aea36
unknown <milkhao0608@gmail.com>
2022-12-03 21:08:02 +0800

    Delete moreview/moreview something ...

--- review/models.py
+++ review/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class review(models.Model):
+    name = models.CharField(max_length=10, default="搞笑")
+    description = models.TextField(default="a")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
