db32d2decc9e702f2fbae4ce230f4dcf97cb2660
hsiuchun <joyce111423048@g.ncu.edu.tw>
2022-12-05 18:01:20 +0800

    update w/ auth

--- users/views.py
+++ users/views.py
@@ -5,3 +5,15 @@ from django.contrib.auth.views import LoginView
 # Create your views here.
 class UserLoginView(LoginView):
     template_name = "login.html"
+'''
+class UserLogoutView(LogoutView):
+    
+class UserRegisterView(RegisterView):
+    template_name = "register.html"
+
+class UserProfileView(ProfileView):
+    template_name ="profile.html"
+
+class UserListView(UserListView):
+    tmplate_name = "user_list.html"
+'''
