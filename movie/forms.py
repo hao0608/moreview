c31323f1a0302f385222b7335a9e6e17ffd0be9b
QuenHengLee <haha89725@gmail.com>
2022-12-05 16:55:41 +0800

    stage

--- movie/forms.py
+++ movie/forms.py
@@ -1,9 +1,23 @@
 from django import forms
 
 from movie.models import Movie
-
-
+ 
 class MovieModelForm(forms.ModelForm):
     class Meta:
         model = Movie
-        fields = "__all__"
+        #fields = "__all__"
+
+ 
+
+        fields = ['tag_id','name','content','official_site','time','grade','date_released']
+
+        widgets = {
+            
+            'name' : forms.TextInput(attrs = {'class':'form-control' ,'required': True}),
+            'content' : forms.Textarea(attrs={'class': 'tinymceTextarea'}),
+            'official_site' : forms.TextInput(attrs = {'class':'form-control'}),
+            'time' : forms.TextInput(attrs = {'class':'form-control'}),
+            'grade' : forms.Select(attrs = {'class':'form-control'}),
+            'date_released' : forms.TextInput(attrs = {'class':'form-control', 'type':'date'})
+
+        } 
