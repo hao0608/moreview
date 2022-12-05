afe2f512c625fee79b60b71361f71731f786b9b1
QuenHengLee <QuenHengLee@users.noreply.github.com>
2022-12-05 08:57:10 +0000

    style: apply linting changes

--- movie/models.py
+++ movie/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
- 
+
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=10, default="搞笑")
@@ -8,13 +8,15 @@ class Tag(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
+
 movie_grade = [
-    ('普遍級','普遍級'),
-    ('保護級','保護級'),
-    ('輔12級','輔12級'),
-    ('輔15級','輔15級'),
-    ('限制級','限制級'),
- ]
+    ("普遍級", "普遍級"),
+    ("保護級", "保護級"),
+    ("輔12級", "輔12級"),
+    ("輔15級", "輔15級"),
+    ("限制級", "限制級"),
+]
+
 
 class Movie(models.Model):
     tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE, default="1")
@@ -23,15 +25,12 @@ class Movie(models.Model):
     official_site = models.TextField(default="a")
     time = models.TextField(default="a")
     # grade = models.TextField(default="普遍級")
-    grade = models.TextField(
-        null=False, blank=False,
-        choices=movie_grade,
-        default=1
+    grade = models.TextField(null=False, blank=False, choices=movie_grade, default=1)
+
+    date_released = models.DateField(
+        default="2020-10-10",
     )
-
-
-    date_released = models.DateField(default="2020-10-10",)
-    #date_released = models.DateField(widget = forms.DateInput(attrs={'type':'date'}))
+    # date_released = models.DateField(widget = forms.DateInput(attrs={'type':'date'}))
 
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
