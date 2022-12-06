5a6f08922931c840be458800f6e4e1a43270e269
YungLin-CHU <lexlie.yunglinchu@gmail.com>
2022-12-06 14:42:50 +0800

    feat: add user logout function

--- moreview/settings.py
+++ moreview/settings.py
@@ -145,3 +145,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
 
 LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
 # from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse
 
 
 # Create your views here.
 class UserLoginView(LoginView):
     template_name = "login.html"
+
+
+class UserLogoutView(LogoutView):
+    http_method_names = ['post']
+
+    def get_redirect_url(self):
+        return ""
