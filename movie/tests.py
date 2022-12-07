b5370574c419d89ac6498eb42a724a85d15c8847
unknown <milkhao0608@gmail.com>
2022-12-08 00:10:34 +0800

    Repair something wrong

--- movie/tests.py
+++ movie/tests.py
@@ -13,28 +13,28 @@ from .views import (
 # Create your tests here.
 
 
-'''
-class MovieListViewTest(TestCase):
-    view=MovieListView()
-    def test_template_is_correct(self):
-        self.assertEquals("movie_list.html",self.view.template_name)
+
+# class MovieListViewTest(TestCase):
+#     view=MovieListView()
+#     def test_template_is_correct(self):
+#         self.assertEquals("movie_list.html",self.view.template_name)
         
-class MovieDetailViewTest(TestCase):
-    view=MovieDetailView()
-    def test_template_is_correct(self):
-        self.assertEquals("movie_detail.html",self.view.template_name)
+# class MovieDetailViewTest(TestCase):
+#     view=MovieDetailView()
+#     def test_template_is_correct(self):
+#         self.assertEquals("movie_detail.html",self.view.template_name)
 
-class MovieCreateViewTest(TestCase):
-    view=MovieCreateView()
-    def test_template_is_correct(self):
-        self.assertEquals("movie_create_form.html",self.view.template_name)
+# class MovieCreateViewTest(TestCase):
+#     view=MovieCreateView()
+#     def test_template_is_correct(self):
+#         self.assertEquals("movie_create_form.html",self.view.template_name)
 
-class MovieDeleteViewTest(TestCase):
-    view=MovieDeleteView()
+# class MovieDeleteViewTest(TestCase):
+#     view=MovieDeleteView()
 
 
-class MovieEditViewTest(TestCase):
-    view=MovieEditView()
-    def test_template_is_correct(self):
-        self.assertEquals("movie_edit_form.html",self.view.template_name)
-"""
+# class MovieEditViewTest(TestCase):
+#     view=MovieEditView()
+#     def test_template_is_correct(self):
+#         self.assertEquals("movie_edit_form.html",self.view.template_name)
+
