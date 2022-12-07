3d82842c06090de6df90f9658742de5cffc40c05
QuenHengLee <haha89725@gmail.com>
2022-12-07 15:55:38 +0800

    stage

--- review/models.py
+++ review/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from movie.models import Movie
 
 # Create your models here.
+
+movie_rate = [
+    ("5", "5"),
+    ("4", "4"),
+    ("3", "3"),
+    ("2", "2"),
+    ("1", "1"),
+]
 class Review(models.Model):
     user_id = models.ManyToManyField(
             User,
@@ -10,8 +18,9 @@ class Review(models.Model):
             through_fields=('review_id', 'user_id')
         )
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    content = models.TextField(default="a")
-    rating = models.CharField(max_length=10, default="5")
+    content = models.TextField(default="a:")
+    # rating = models.CharField(max_length=10, default="5")
+    rating = models.TextField(null=False, blank=False, choices=movie_rate, default=5)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
