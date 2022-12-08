76b4d79544f461c32168bb312b3860394c7969e6
YungLin-CHU <YungLin-CHU@users.noreply.github.com>
2022-12-08 06:32:54 +0000

    style: apply linting changes

--- moreview/settings.py
+++ moreview/settings.py
@@ -120,9 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "zh-Hant"
 
-LOCALE_PATHS = [
-    BASE_DIR / 'locale'
-]
+LOCALE_PATHS = [BASE_DIR / "locale"]
 
 TIME_ZONE = "Asia/Taipei"
 
--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path('register', views.UserRegisterView.as_view(), name="register"),
+    path("register", views.UserRegisterView.as_view(), name="register"),
     path("login", views.UserLoginView.as_view(), name="login"),
     path("logout", views.UserLogoutView.as_view(), name="logout"),
 ]
