6561244fc60a649fd40d2b7689f09aa52cfa3d2c
YungLin-CHU <YungLin-CHU@users.noreply.github.com>
2022-12-15 07:42:41 +0000

    style: apply linting changes

--- users/migrations/0005_alter_user_date_updated.py
+++ users/migrations/0005_alter_user_date_updated.py
@@ -5,13 +5,13 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('users', '0004_alter_user_groups_alter_user_user_permissions'),
+        ("users", "0004_alter_user_groups_alter_user_user_permissions"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='user',
-            name='date_updated',
+            model_name="user",
+            name="date_updated",
             field=models.DateTimeField(auto_now=True),
         ),
     ]
