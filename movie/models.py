ee70a02e2bcee0732e53ae48d45f5b12012dbc2f
YungLin-CHU <lexlie.yunglinchu@gmail.com>
2022-12-15 14:49:16 +0800

    refactor: refactor movie_create_form.html to use crispy and use locale

--- movie/models.py
+++ movie/models.py
@@ -1,11 +1,14 @@
+import datetime
+
 from django.db import models
 from django.urls import reverse
-import datetime
+from django.utils.translation import gettext as _
+
 
 # Create your models here.
 class Tag(models.Model):
-    name = models.CharField(max_length=10, default="搞笑")
-    description = models.TextField(default="a")
+    name = models.CharField(_('name'), max_length=10)
+    description = models.TextField(_('description'))
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
@@ -23,16 +26,14 @@ movie_grade = [
 
 
 class Movie(models.Model):
-    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE, default="1")
-    name = models.CharField(max_length=20)
-    content = models.TextField(max_length=500)
-    official_site = models.TextField()
-    time = models.IntegerField()
-    image = models.ImageField(upload_to="movies/", blank=False, null=False)
-    grade = models.TextField(null=False, blank=False, choices=movie_grade, default=1)
-    date_released = models.DateField(
-        default=datetime.date.today,
-    )
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE, verbose_name=_('tag'))
+    name = models.CharField(_('name'), max_length=20)
+    content = models.TextField(_('content'), max_length=500)
+    official_site = models.URLField(_('official site'))
+    time = models.PositiveSmallIntegerField(_('time'))
+    image = models.ImageField(_('image'), upload_to="movies/")
+    grade = models.TextField(_('grade'), choices=movie_grade)
+    date_released = models.DateField(_('release date'), default=datetime.date.today)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
