bcc52070c2715565e016817f8fc731733b55f24e
hsiuchun <joyce111423048@g.ncu.edu.tw>
2022-12-23 16:42:58 +0800

    fixed bugs

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.password_validation import password_validators_help_text_html
 from django.contrib.auth.password_validation import validate_password
 from django.utils.translation import gettext as _
+from django.core.validators import RegexValidator
 
 from .models import User
 
@@ -51,6 +52,8 @@ class ProfileUpdateForm(forms.ModelForm):
 
 
 class AdminCreateForm(forms.ModelForm):
+    username = forms.CharField(max_length=150, validators=[RegexValidator(
+        '([a-z]|[A-Z]|[0-9]|[@|.|-|_|+])', message="Enter a Valid Indian GST Number")])
     password = forms.CharField(
         label=_("password"), widget=forms.PasswordInput(), max_length=128
     )
