9689ac7be21b2aab251d56ce2fd39ba9ecaff9f8
hsiuchun <joyce111423048@g.ncu.edu.tw>
2022-12-24 17:51:35 +0800

    fixed email error messages

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext as _
 
-
 # Create your models here.
 class User(AbstractUser):
     first_name = models.CharField(_("first name"), max_length=150)
